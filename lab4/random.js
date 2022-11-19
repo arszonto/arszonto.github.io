76be002e69d7cd2484329d8b862b03e15b6dacf8
arszonto <arszonto@gmail.com>
2022-11-19 18:58:24 +0100

    Lab4 - Skrypt z losowymi danymi

---
+++ lab4/random.js
@@ -0,0 +1,8 @@
+function fill_with_random_data() {
+    document.getElementById('name').value="Frank";
+    document.getElementById('surname').value="Underwood";
+    document.getElementById('email').value="test@edu.p.lodz.pl";
+    document.getElementById('postal_code').value="90-149";
+    document.getElementById('nip').value="111-222-33-44";
+    document.getElementById('phone').value="666-213-711";
+}
